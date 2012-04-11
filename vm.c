d817dfea38f10d5581edeedbb043d37e05a4119e
Michael Westbom <totallymike@gmail.com>
2012-04-10 22:40:54 -0400

    Rearranged much.
    Should make it a bit easier to handle additions later.

--- vm.c
+++ vm.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <signal.h>
 
+/* A and B equate to how far to shift
+ * to get that section of the current word.*/
+#define A 4
+#define B 10
+
 #define SET 0x1
 #define ADD 0x2
 #define SUB 0x3
@@ -20,38 +25,101 @@
 
 typedef unsigned short u16;
 
+u16 registers[8];
+u16 PC = 0, SP = 0, O = 0;
+u16 ram[0x10000];
+int iCycles;
+
 u16 get_op(u16 word) {
   return (word & 0xf);
 }
 
-u16 get_a(u16 word) {
-  return ((word >> 4) & 0x3f);
+u16 *get_dest(u16 word, u16 shift) {
+  u16 a = (word >> shift) & 0x3f;
+  if (a <= 0x07)
+    return &registers[a];
+  else if (a >= 0x08 && a <= 0x0f) {
+    iCycles++;
+    return &ram[ registers[ a - 0x08 ] ];
+  } else if (a >= 0x10 && a <= 0x17) {
+    iCycles++;
+    return &ram[ ram[PC++] + registers[ a - 0x10 ] ];
+  }
+  switch(a) {
+    case 0x18:
+      return &ram[ SP++ ];
+    case 0x19:
+      return &ram[ SP ];
+    case 0x1a:
+      return &ram[ --SP ];
+    case 0x1b:
+      return &SP;
+    case 0x1c:
+      return &PC;
+    case 0x1d:
+      return &O;
+    case 0x1e:
+      iCycles++;
+      return &ram[ ram[PC++] ];
+    default:
+      return NULL;
+  }
 }
 
-u16 get_b(u16 word) {
-  return ((word >> 10) & 0x3f);
+u16 get_val(u16 word, u16 shift) {
+  u16 b = (word >> shift) & 0x3f;
+  if (b <= 0x07)
+    return registers[b];
+  else if (b >= 0x08 && b <= 0x0f) {
+    iCycles++;
+    return ram[ registers[b - 0x08] ];
+  } else if (b >= 0x10 && b <= 0x17) {
+    iCycles++;
+    return ram[ ram[PC++] + registers[b - 0x10] ];
+  } else if (b >= 0x20 && b <= 0x3f)
+    return b - 0x20;
+
+  switch(b) {
+    case 0x18:
+      return ram[ SP++ ];
+    case 0x19:
+      return ram[ SP ];
+    case 0x1a:
+      return ram[ --SP ];
+    case 0x1b:
+      return SP;
+    case 0x1c:
+      return PC;
+    case 0x1d:
+      return O;
+    case 0x1e:
+      iCycles++;
+      return ram[ ram[PC++] ];
+    case 0x1f:
+      iCycles++;
+      return ram[PC++];
+    default:
+      break;
+  }
+}
+
+u16 steps_forward() {
+  u16 steps = 1;
+  u16 a = (ram[PC] >> A) & 0x3f;
+  u16 b = (ram[PC] >> B) & 0x3f;
+
+  if ((a >= 0x08 && a <= 0x17) ||
+      (a >= 0x1e && a <= 0x1f))
+    steps++;
+  if ((b >= 0x08 && b <= 0x17) ||
+      (b >= 0x1e && b <= 0x1f))
+    steps++;
+  return steps;
 }
 
 int sigint_received = 0;
 void stop_cpu(int signum) {
   sigint_received = 1;
-}
-
-void dump(u16* PC, u16* current, u16* SP, u16* registers, u16* ram) {
-  int i;
-  u16 stack = *SP;
-  char reg_list[] = "ABCXYZIJ";
-
-  printf("Current word: %#.4x\n", *current);
-
-  printf("Registers:\n");
-  for (i = 0; i < 8; i++) {
-    printf("\t%c: %#.4x\n", reg_list[i], registers[i]);
-  }
-  printf("\nStack:\n");
-  while (stack != 0) {
-    printf("\t%#.4x: %#.4x\n", stack, ram[stack++]);
-  }
 }
 
 void printUsage(char *fileName) {
@@ -60,12 +128,15 @@ void printUsage(char *fileName) {
 }
 
 int main(int argc, char *argv[]) {
-  u16 command;
-  u16 cur;
-  u16 ram[0x10000];
   char reg_names[] = "ABCXYZIJ";
-  char *isDebug = getenv("DEBUG");
-  int maxCycles = 0;
+  char *fileName = argv[1];
+
+  u16 *target;
+  u16 value;
+  u16 current;
+
+  unsigned int a, b, i;
+  unsigned int cycles = 0;
 
   if (argc < 2) {
     printUsage(argv[0]);
@@ -78,27 +149,6 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   
-  char *fileName = argv[argc-1];
-  if (strcmp(argv[1], "-n") == 0) {
-    if (argc < 4) {
-      printUsage(argv[0]);
-      return 0;
-    } else {
-      maxCycles = atoi(argv[2]);
-    }
-  }
-
-
-  /* Registers:
-   *  0x00 - 0x07 -> A,B.C,X,Y,Z,I,J
-   *  Not named, but easier to address!
-   */
-  u16 registers[8];
-  u16 PC = 0, SP = 0, O = 0;
-  u16 *target;
-  u16 value;
-  unsigned int a, b, i;
-  unsigned int cycles = 0;
   FILE *fp;
   fp = fopen(fileName, "rb");
   if (fp == NULL) {
@@ -106,259 +156,180 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   else {
+    /* Program loader. */
     while (!feof(fp)) {
       fread(&ram[PC++], 2, 1, fp);
     }
   }
   fclose(fp);
-  a = PC - 1;
   PC = 0;
   
   signal(SIGINT, stop_cpu);
-  if (!maxCycles)
-    printf("Beginning emulation.  Press Ctrl+C to exit\n");
-  while (1) {
+  
+  while(1) {
     cycles++;
-    cur = ram[PC++];
-    command = get_op(cur);
-    a = get_a(cur);
-    b = get_b(cur);
-    if (isDebug != NULL) {
-      printf("Cycle: %d ", cycles);
-      printf("Current: %#.4x ", cur);
-      printf("Command: %#.2x\n", command);
-    }
+    current = ram[PC++];
+    switch(get_op(current)) {
 
-    if (command == 0) {
-      if (a == 0x01) {
-        cycles++;
-        ram[ --SP ] = PC;
-        if (b < 0x07) {
-          value = registers[b];
-        } else if (b > 0x07 && b < 0x10) {
-          value = ram[ registers[ b - 0x08 ] ];
-        } else if (b > 0x0f && b < 0x18) {
-          value = ram[ ram[ PC++ ] + registers[ b - 0x10 ] ];
-        } else if (b == 0x18) {
-          value = ram[ SP++ ];
-        } else if (b == 0x19) {
-          value = ram[ SP ];
-        } else if (b == 0x1a) {
-          value = ram[ --SP ];
-        } else if (b == 0x1b) {
-          value = SP;
-        } else if (b == 0x1c) {
-          value = PC;
-        } else if (b == 0x1d) {
-          value = O;
-        } else if (b == 0x1e) {
-          value = ram[ ram[ PC++ ] ];
-          cycles++;
-        } else if (b == 0x1f) {
-          value = ram[ PC++ ];
-          cycles++;
-        } else if (b > 0x1f && b < 0x40) {
-          value = (b - 0x20);
+      case 0x0:
+        // TODO: Don't depend on get_val here.
+        // If Notch moves above 0xf non-basic opcodes
+        // this will add an extra cycle
+        switch(get_val(current, A)) {
+          case 0x01:
+            iCycles = 2;
+            ram[--SP] = PC;
+            PC = *get_dest(current, B);
+            break;
+          default:
+            break;
         }
-        PC = value;
-      }
-      continue;
-    }
+        break;
+        // End special opcodes
 
-    if (a < 0x08) {
-      target = &registers[a];
-    } else if (a > 0x07 && a < 0x10) {
-      target = &ram[ registers[ a - 0x08 ] ];
-    } else if (a > 0x0f && a < 0x18) {
-      target = &ram[ ram[PC++] + registers[a - 0x10] ];
-      cycles++;
-    } else if (a == 0x18) {
-      target = &ram[ SP++ ];
-    } else if (a == 0x19) {
-      target = &ram[SP];
-    } else if (a == 0x1a) {
-      target = &ram[--SP];
-    } else if (a == 0x1b) {
-      target = &SP;
-    } else if (a == 0x1c) {
-      target = &PC;
-    } else if (a == 0x1d) {
-      target = &O;
-    } else if (a == 0x1e) {
-      target = &ram[ram[PC++]];
-      cycles++;
-    } else {
-      continue;
-    }
-
-    if (b < 0x07) {
-      value = registers[b];
-    } else if (b > 0x07 && b < 0x10) {
-      value = ram[ registers[ b - 0x08 ] ];
-    } else if (b > 0x0f && b < 0x18) {
-      value = ram[ ram[ PC++ ] + registers[ b - 0x10 ] ];
-    } else if (b == 0x18) {
-      value = ram[ SP++ ];
-    } else if (b == 0x19) {
-      value = ram[ SP ];
-    } else if (b == 0x1a) {
-      value = ram[ --SP ];
-    } else if (b == 0x1b) {
-      value = SP;
-    } else if (b == 0x1c) {
-      value = PC;
-    } else if (b == 0x1d) {
-      value = O;
-    } else if (b == 0x1e) {
-      value = ram[ ram[ PC++ ] ];
-      cycles++;
-    } else if (b == 0x1f) {
-      value = ram[ PC++ ];
-      cycles++;
-    } else if (b > 0x1f && b < 0x40) {
-      value = (b - 0x20);
-    }
-
-    switch(get_op(cur)) {
       case SET:
-        *target = value;
+        iCycles = 1;
+        *get_dest(current, A) = get_val(current, B);
         break;
+
       case ADD:
-        cycles++;
-        a = *target + value;
-        if (a > 0xffff) {
-          *target = (a & 0xffff);
-          O = 0x0001;
-        } else {
-          *target = (u16) a;
-        }
+        iCycles = 2;
+        target = get_dest(current, A);
+        value = *target + get_val(current, B);
+        if (value > 0xffff)
+          O = 1;
+        else
+          O = 0;
+        *target = value & 0xffff;
         break;
+
       case SUB:
-        cycles++;
-        a = *target - value;
-        if (a > 0xffff) {
-          *target = (a & 0xffff);
+        iCycles = 2;
+        target = get_dest(current, A);
+        value = *target - get_val(current, B);
+        if (value > 0xffff)
           O = 0xffff;
-        } else {
-          *target = (u16) a;
-        }
+        else
+          O = 0;
+        *target = value & 0xffff;
         break;
+
       case MUL:
-        cycles++;
-        O = ((*target * value) >> 0x10) & 0xffff;
+        iCycles = 2;
+        target = get_dest(current, A);
+        value = get_val(current, B);
         *target *= value;
+        O = ((*target * value)>>16) & 0xffff;
         break;
+
       case DIV:
-        cycles += 2; // Div takes 3 cycles.
+        iCycles = 3;
+        target  = get_dest(current, A);
+        value   = get_val(current, B);
+
         if (value == 0) {
           *target = 0;
-        } else {
-          O = ((*target << 16)/value) & 0xffff;
-          *target /= value;
+          O = 0;
+          break;
         }
+        *target /= value;
+        O = ((*target<<16)/value) & 0xffff;
         break;
+
       case MOD:
-        cycles += 2;
+        iCycles = 3;
+        target  = get_dest(current, A);
+        value   = get_val (current, B);
+
         if (value == 0) {
           *target = 0;
-        } else {
-          *target %= value;
+          O = 0;
+          break;
         }
+        
+        *target %= value;
         break;
+
       case SHL:
-        cycles++;
-        O = ((*target << value) >> 0x10) & 0xffff;
+        iCycles = 2;
+        target  = get_dest(current, A);
+        value   = get_val (current, B);
+
         *target = *target << value;
+
+        O = ((*target << value) >> 16) & 0xffff;
         break;
+
       case SHR:
-        cycles++;
-        O = ((*target >> 16) >> value) & 0xffff;
+        iCycles = 2;
+        target  = get_dest(current, A);
+        value   = get_val (current, B);
+
         *target = *target >> value;
+
+        O = ((*target << 16) >> value) & 0xffff;
         break;
+
       case AND:
-        *target = *target & value;
+        iCycles = 1;
+        *get_dest(current, A) &= get_val(current, B);
         break;
+
       case BOR:
-        *target = *target | value;
+        iCycles = 1;
+        *get_dest(current, A) |= get_val(current, B);
         break;
+
       case XOR:
-        *target = *target ^ value;
+        iCycles = 1;
+        *get_dest(current, A) ^= get_val(current, B);
         break;
+
       case IFE:
-        cycles++;
-        // If a value == b value, proceed.  Otherwise skip next instruction.
-        if (*target == value) {
-          break;
-        } else {
-          if ((get_a(ram[PC]) > 0x07) && (get_a(ram[PC]) < 0x18) ||
-              (get_a(ram[PC]) > 0x1d) && (get_a(ram[PC]) < 0x20)) 
-            PC++;
-          if ((get_b(ram[PC]) > 0x07) && (get_b(ram[PC]) < 0x18) ||
-              (get_b(ram[PC]) > 0x1d) && (get_b(ram[PC]) < 0x20)) 
-            PC++;
-          PC++;
-        }
+        iCycles = 2;
+
+        if (get_val(current, A) != get_val(current, B))
+          PC += steps_forward();
+        else
+          iCycles++;
         break;
+
       case IFN:
-        cycles++;
-        // Inverse of IFE
-        if (*target != value) {
-          break;
-        } else {
-          if ((get_a(ram[PC]) > 0x07) && (get_a(ram[PC]) < 0x18) ||
-              (get_a(ram[PC]) > 0x1d) && (get_a(ram[PC]) < 0x20)) 
-            PC++;
-          if ((get_b(ram[PC]) > 0x07) && (get_b(ram[PC]) < 0x18) ||
-              (get_b(ram[PC]) > 0x1d) && (get_b(ram[PC]) < 0x20)) 
-            PC++;
-          PC++;
-        }
+        iCycles = 2;
+
+        if (get_val(current, A) == get_val(current, B))
+          PC += steps_forward();
+        else
+          iCycles++;
         break;
+
       case IFG:
-        cycles++;
-        if (*target > value) {
-          break;
-        } else {
-          if ((get_a(ram[PC]) > 0x07) && (get_a(ram[PC]) < 0x18) ||
-              (get_a(ram[PC]) > 0x1d) && (get_a(ram[PC]) < 0x20)) 
-            PC++;
-          if ((get_b(ram[PC]) > 0x07) && (get_b(ram[PC]) < 0x18) ||
-              (get_b(ram[PC]) > 0x1d) && (get_b(ram[PC]) < 0x20)) 
-            PC++;
-          PC++;
-        }
+        iCycles = 2;
+
+        if (get_val(current, A) < get_val(current, B))
+          PC += steps_forward();
+        else
+          iCycles++;
         break;
+
       case IFB:
-        cycles++;
-        if ((*target & value) != 0) {
-          break;
-        } else {
-          if ((get_a(ram[PC]) > 0x07) && (get_a(ram[PC]) < 0x18) ||
-              (get_a(ram[PC]) > 0x1d) && (get_a(ram[PC]) < 0x20)) 
-            PC++;
-          if ((get_b(ram[PC]) > 0x07) && (get_b(ram[PC]) < 0x18) ||
-              (get_b(ram[PC]) > 0x1d) && (get_b(ram[PC]) < 0x20)) 
-            PC++;
-          PC++;
-        }
+        iCycles = 2;
+
+        if ((get_val(current, A) & get_val(current, B)) == 0)
+          PC += steps_forward();
+        else
+          iCycles++;
         break;
-      default:
-        printf("Crapped out\n");
-        return 1;
-    }
-    if (maxCycles && cycles >= maxCycles) {
+    } // End of opcodes.
+    if (sigint_received)
       break;
-    }
-    if (sigint_received == 1) {
-      break;
-    }
   }
 
   printf("Cycles: %d\n", cycles);
   printf("Registers:\n");
-  for (i = 0; i < 8; i++)
-    printf("\t%c:\t%#.4x\n", reg_names[i], registers[i]);
-  printf("Good-bye!\n");
+  for (i = 0; i < 8; i++) {
+    printf("\t%c: %#.4x\n", reg_names[i], registers[i]);
+  }
 
   return 0;
 }
